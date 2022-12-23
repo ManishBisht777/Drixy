5d38092ceb545c372c539e727f44dd168809274a
manish bisht <manishbisth9711@gmail.com>
2022-12-23 23:11:08 +0530

    feat : home page

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import logo from "../../images/artsy.svg";
+import { Link, NavLink } from "react-router-dom";
+import ham from "../../images/ham.svg";
+import close from "../../images/close.svg";
 
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const toggleNavbar = (e) => {
+    e.stopPropagation();
+    console.log(e.target.tagName);
+    setToggleNav(!toggleNav);
+  };
+
   return (
-    <nav>
-      i am nav bar
-      <p>hello</p>
-      <ul>
-        <Link to="/explore">explore</Link>
-        <Link to="/">home</Link>
+    <nav className="navbar container container--between">
+      <button
+        className="navbar__hamburger"
+        id="togglemenu"
+        aria-expanded={toggleNav}
+        aria-label="toggle-menu"
+        onClick={() => setToggleNav(!toggleNav)}
+      >
+        <img src={ham} alt="toggle menu" />
+      </button>
+      <img className="navbar__logo" src={logo} alt="Artsy logo" />
+      <ul
+        className="container gap--lg  navbar__NavLinks navlink-link"
+        onClick={(e) => {
+          toggleNavbar(e);
+        }}
+      >
+        <li>
+          <img className="navbar__logo-mobile" src={logo} alt="Artsy logo" />
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active--link" : "")}
+            to="/"
+          >
+            home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active--link" : "")}
+            to="/marketplace"
+          >
+            marketplace
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active--link" : "")}
+            to="/auction"
+          >
+            auctiion
+          </NavLink>
+        </li>
       </ul>
+      <ul className="container gap--md navbar__NavLinks ">
+        <li>
+          <Link className="button--secondary" to="/notification">
+            Login
+          </Link>
+        </li>
+      </ul>
+
+      <button className="nav__close" onClick={() => setToggleNav(!toggleNav)}>
+        <img src={close} alt="nav close" />
+      </button>
     </nav>
   );
 };
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import what from "../../images/what.jpg";
 
 const Home = () => {
   return (
@@ -9,23 +10,45 @@ const Home = () => {
         <button className="button--primary">Explore</button>
       </section>
 
-      <section className="container what">
-        <img src="" alt="what it is" />
-        <div>
+      <section className="container what gap--lg">
+        <div className="image-container">
+          <img src={what} alt="" />
+        </div>
+        <div className="container container--col gap--md">
           <h2>what it is</h2>
+          <ul className="container container--col gap--sm">
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+              amet!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+              amet!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+              amet!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+              amet!
+            </li>
+          </ul>
           <p>
             We provide you a large collection on compoents for you to mix and
             match to make a desgifn for you upcoming project
           </p>
-          <button className="button--primary">See some example</button>
+          <button className="button--primary">examples</button>
         </div>
       </section>
-      <section className="example">
+      <section className="example container container--center gap--lg">
         <h3>here are some of design made my users</h3>
-        <div className="">
-          <img src="" alt="imgages here" />
-          <img src="" alt="imgages here" />
-          <img src="" alt="imgages here" />
+        <div className="example__images container gap--md container--center-row">
+          <img src={what} alt="imgages here" />
+          <img src={what} alt="imgages here" />
+          <img src={what} alt="imgages here" />
+          <img src={what} alt="imgages here" />
+          <img src={what} alt="imgages here" />
         </div>
       </section>
     </main>
